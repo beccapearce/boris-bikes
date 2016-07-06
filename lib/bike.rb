18d72e4912b0496f88e90a6908ec46f6e2ee4580
Thady Condon <thadycondon@hotmail.com>
2016-07-06 11:14:42 +0100

    Step 9

---
+++ lib/bike.rb
@@ -0,0 +1,5 @@
+class Bike
+  def working?
+    puts "Working"
+  end
+end
--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,9 @@
+require './bike.rb'
+
 class DockingStation
 
 def release_bike
-  puts "Release"
+  bike = Bike.new
+  bike.working?
 end
-
 end
